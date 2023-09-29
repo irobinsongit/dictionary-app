d174407f27d36424a0cc8af345e16031a1f04daf
irobinsongit <isobellepandas@gmail.com>
2023-09-29 13:53:57 +0100

    Updated the meaning.

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,18 +5,21 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+
       {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <span>
-              <strong>Definition: </strong> {definition.definition}
-              <br />
-              <strong>Example: </strong>
-              <em>{definition.example}</em>
-              <Synonyms synonyms={definition.synonyms} />
-            </span>
-          </div>
-        );
+        if (index <= 1) {
+          return (
+            <div key={index}>
+              <span>
+                <strong>Definition: </strong> {definition.definition}
+                <br />
+                <strong>Example: </strong>
+                <em>{definition.example}</em>
+                <Synonyms synonyms={definition.synonyms} />
+              </span>
+            </div>
+          );
+        }
       })}
     </div>
   );
