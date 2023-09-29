31d6c4736d4d3bca1879ec27c4f14fb094be82ae
irobinsongit <isobellepandas@gmail.com>
2023-09-29 09:33:20 +0100

    Added basic search engine.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from "./logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Coded by Izzy and this code is{" "}
+          <a
+            href="https://github.com/irobinsongit/dictionary-app"
+            target="_blank"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
